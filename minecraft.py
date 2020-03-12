9af04be644c83098cb2abccf000b9a15c67d91f3
Cleo Credo <credocleo28@gmail.com>
2020-03-13 07:41:04 +0800

    initial files

---
+++ minecraft.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+mc.postToChat("Hello World")
+
+#x, y, z = mc.player.getPos()
+#mc.setBlock(x+1, y, z ,1)
+#mc.setBlock(x+1, y, z, 2)
+
+#mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, 1)
+
+#tnt
+#mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, 46, 1)
